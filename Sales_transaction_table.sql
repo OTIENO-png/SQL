02492a2a5180ca97fd2e107dea80df6e709c8eaf
brian <otienolubalo@gmail.com>
2025-03-21 12:59:04 +0300

    Created a Sales_transaction_table using sql

---
+++ Sales_transaction_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Sales_Transactions (
+    OrderDate VARCHAR(50),
+    Region VARCHAR(50),
+    Rep VARCHAR(50) ,
+    Item VARCHAR(50) ,
+    Units INT,
+    UnitCost DECIMAL(10,2),
+    Total DECIMAL(10,2) 
+);
