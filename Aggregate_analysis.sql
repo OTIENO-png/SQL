53f36a0f4f21879266db256862d502dac56988e8
brian <otienolubalo@gmail.com>
2025-03-25 21:36:23 +0300

    Modified the code so that the results could be rounded off to 2dp

--- Aggregate_analysis.sql
+++ Aggregate_analysis.sql
@@ -1,2 +1,2 @@
-SELECT MAX(UnitCost)AS Maximum_UnitCost, MIN(UnitCost)AS Minimum_UnitCost, AVG(UnitCost) AS Average_Unitcost, STD(UnitCost)AS Unit_cost_Starndard_deviation, SUM(Total) AS Totals
+SELECT MAX(UnitCost)AS Maximum_UnitCost, MIN(UnitCost)AS Minimum_UnitCost, ROUND(AVG(UnitCost), 2) AS Average_UnitCost, ROUND(STD(UnitCost),2)AS Unit_cost_Starndard_deviation, ROUND(SUM(Total),2) AS Totals
 FROM Sales_transaction;
